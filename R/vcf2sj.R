1b2b1e1778c77943d6a5e4195f563e8b7ebb8089
Qiang Hu <qiang.hu@roswellpark.org>
2021-08-13 12:15:52 -0400

    update vcf2sj

--- R/vcf2sj.R
+++ R/vcf2sj.R
@@ -1,8 +1,10 @@
 #' annotated VCF to SJ format
-#' @param avcf A vcf from VEP
+#' @param avcf A vcf from VEP.
+#' @param tidx The tumor sample index.
+#' @param nidx The normal sample index.
 #' @export
-vcf2sj <- function(avcf){
-    vcf1 <- readVcf(avcf)
+vcf2sj <- function(avcf, tidx = 1, nidx = 2){
+    vcf1 <- expand(readVcf(avcf))
     tid <- colnames(vcf1)[1]
     gann <- lapply(info(vcf1)$CSQ, function(x){
         idx <- grep("RefSeq", x)[1]
@@ -16,8 +18,8 @@ vcf2sj <- function(avcf){
     AA <- paste0(gann[,"Protein_position"], ":", gann[,"Amino_acids"])
 
     AD <- geno(vcf1)$AD
-    AD_T <- do.call(rbind, AD[,1])
-    AD_N <- do.call(rbind, AD[,2])
+    AD_T <- rbind(AD[,tidx,])
+    AD_N <- rbind(AD[,nidx,])
     Ref <- as.character(ref(vcf1))
     Mut <- unlist(lapply(alt(vcf1), function(x)as.character(x)[1]))
 
@@ -25,7 +27,7 @@ vcf2sj <- function(avcf){
 
     data.frame(GeneName=gann[,"SYMBOL"], SJQuality=Qual, Sample = tid, Chr=as.character(seqnames(vcf1)),
                WU_HG19_Pos=start(vcf1), Class=gann[,"Consequence"], AAChange=AA, ProteinGI=NA, mRNA_acc=NM,
-               "#Mutant_In_Tumor"=AD_T[,2], "#Total_In_Tumor"=rowSums(AD_T),
-               "#Mutant_In_Normal"=AD_N[,2], "#Total_In_Normal"=rowSums(AD_N),
+               "#Mutant_In_Tumor"=AD_T[,2], "#Total_In_Tumor"=rowSums(AD_T, na.rm=TRUE),
+               "#Mutant_In_Normal"=AD_N[,2], "#Total_In_Normal"=rowSums(AD_N, na.rm=TRUE),
                ReferenceAllele=Ref, MutantAllele=Mut, check.names = F)
 }
